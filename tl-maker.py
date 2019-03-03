a6f8a411030e43c9b10b65452fb59959edb8b3d8
KKadikis <k00kadikis@gmail.com>
2019-03-03 15:09:16 +0200

    Create function stubs for translation searching, parsing and storing

--- tl-maker.py
+++ tl-maker.py
@@ -5,7 +5,7 @@ import argparse
 import time
 
 class TranslationMaker(object):
-    def _recurse(self, parent_path, file_list, level):
+    def loopThroughFileStructure(self, parent_path, file_list, level):
         if len(file_list) == 0 \
             or (self.max_level != -1 and self.max_level <= level):
             return
@@ -21,12 +21,12 @@ class TranslationMaker(object):
 
                 # If current full path is of file
                 if os.path.isfile(full_path):
-                    print(full_path)
+                    self.parseFile(full_path)
 
                 # If sub directory is not in excluded path, traverse it
                 if os.path.isdir(full_path) and sub_path not in self.exf:
                     # print(full_path)
-                    self._recurse(full_path, os.listdir(full_path), level + 1)
+                    self.loopThroughFileStructure(full_path, os.listdir(full_path), level + 1)
 
     def make(self, args):
         self.root = args.root
@@ -34,9 +34,41 @@ class TranslationMaker(object):
         self.exn = args.exclude_name
         self.max_level = args.max_level
 
-        self._recurse(self.root, os.listdir(self.root), 0)
+        self.loopThroughFileStructure(self.root, os.listdir(self.root), 0)
 
         return 'end'
+
+    def parseFile(self, full_path):
+        # Loop through file and find translation usage, noting location of usage if unable to write it
+        translations = self.getTranslationUsage(full_path)
+        if len(translations) == 0:
+            print('No translations found in ' + full_path)
+            return 
+
+        # Prepare translation string
+        translations = self.prepareTranslationStrings(translations)
+
+        # Write to target translation file,creating structure as necessary
+        unstored_translations = self.storeTranslations(translations)
+
+        # Inform user about unstored translations
+        if unstored_translations.length > 0:
+            self.informOfUnstoredTranslations(unstored_translations)
+
+    def getTranslationUsage(self, full_path):
+        translations = []
+
+        return translations
+
+    def prepareTranslationStrings(self, translations):
+        return translations
+
+    def storeTranslations(self, translations):
+        print(translations)
+        return translations
+
+    def informOfUnstoredTranslations(self, unstored_translations):
+        print(unstored_translations)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
