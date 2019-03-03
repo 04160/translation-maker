5dd7cd957c05b190ef07fceeae7145876eed64e9
KKadikis <k00kadikis@gmail.com>
2019-03-03 18:23:04 +0200

    Make test cases harder

--- tests/app/Http/Controllers/Controller.php
+++ tests/app/Http/Controllers/Controller.php
@@ -17,14 +17,15 @@ class Controller extends BaseController
             'key_one' => 'value_one',
             'key_two' => 'value_two'
         ];
-        $var_three = "value_three";
-        $var_four = 'value_four';
+        $var_three = "value_three"; #another comment
+        $var_four = 'value_four'; //comment
+        $translation_arr = [ __('translation.first_one'), trans('translation.second_one') ];
         $translations = [
-            __('underscored.simpletext'),
-            __("underscored.$var_three"),
-            __('underscored.' . $var_four),
-            __('underscored.' . $array_var['key_one']),
-            __('underscored.' . $array_var["key_two"])
+            __('translation.simpletext'),
+            __("translation.$var_three"),
+            __('translation.' . $var_four),
+            __('translation.' . $array_var['key_one']),
+            __('translation.' . $array_var["key_two"])
         ];
     }
 }
