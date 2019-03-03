02f8a536fb2d30a1419f8eb1b1bbcb10a777aee1
KKadikis <k00kadikis@gmail.com>
2019-03-03 13:45:34 +0200

    Change base program to loop through permitted directories and print filenames

--- tl-maker.py
+++ tl-maker.py
@@ -1,36 +1,32 @@
-"""FileTreeMaker.py: ..."""
-
-__author__  = "legendmohe"
+"""TranslationMaker.py: ..."""
 
 import os
 import argparse
 import time
 
-class FileTreeMaker(object):
-    def _recurse(self, parent_path, file_list, prefix, output_buf, level):
+class TranslationMaker(object):
+    def _recurse(self, parent_path, file_list, level):
         if len(file_list) == 0 \
             or (self.max_level != -1 and self.max_level <= level):
             return
         else:
+            # Loop through current file list
             file_list.sort(key=lambda f: os.path.isfile(os.path.join(parent_path, f)))
-            for idx, sub_path in enumerate(file_list):
+            for _, sub_path in enumerate(file_list):
+                # If file or directory is of excluded name, skip it
                 if any(exclude_name in sub_path for exclude_name in self.exn):
                     continue
 
                 full_path = os.path.join(parent_path, sub_path)
-                idc = "┣━"
-                if idx == len(file_list) - 1:
-                    idc = "┗━"
 
+                # If current full path is of file
+                if os.path.isfile(full_path):
+                    print(full_path)
+
+                # If sub directory is not in excluded path, traverse it
                 if os.path.isdir(full_path) and sub_path not in self.exf:
-                    output_buf.append("%s%s[%s]" % (prefix, idc, sub_path))
-                    if len(file_list) > 1 and idx != len(file_list) - 1:
-                        tmp_prefix = prefix + "┃  "
-                    else:
-                        tmp_prefix = prefix + "    "
-                    self._recurse(full_path, os.listdir(full_path), tmp_prefix, output_buf, level + 1)
-                elif os.path.isfile(full_path):
-                    output_buf.append("%s%s%s" % (prefix, idc, sub_path))
+                    # print(full_path)
+                    self._recurse(full_path, os.listdir(full_path), level + 1)
 
     def make(self, args):
         self.root = args.root
@@ -38,18 +34,9 @@ class FileTreeMaker(object):
         self.exn = args.exclude_name
         self.max_level = args.max_level
 
-        print("root:%s" % self.root)
+        self._recurse(self.root, os.listdir(self.root), 0)
 
-        buf = []
-        path_parts = self.root.rsplit(os.path.sep, 1)
-        buf.append("[%s]" % (path_parts[-1],))
-        self._recurse(self.root, os.listdir(self.root), "", buf, 0)
-
-        output_str = "\n".join(buf)
-        if len(args.output) != 0:
-            with open(args.output, 'w') as of:
-                of.write(output_str)
-        return output_str
+        return 'end'
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -60,4 +47,4 @@ if __name__ == "__main__":
     parser.add_argument("-m", "--max_level", help="max level",
                         type=int, default=-1)
     args = parser.parse_args()
-    print(FileTreeMaker().make(args))
+    print(TranslationMaker().make(args))
